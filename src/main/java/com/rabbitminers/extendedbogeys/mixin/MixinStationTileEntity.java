24c55ea72a1687559739d143e9e60a6422322c88
Charlie Wood <rabbitminers2.0@gmail.com>
2023-01-21 23:23:38 +0000

    Refactored MixinStationTileEntity

--- src/main/java/com/rabbitminers/extendedbogeys/mixin/MixinStationTileEntity.java
+++ src/main/java/com/rabbitminers/extendedbogeys/mixin/MixinStationTileEntity.java
@@ -3,8 +3,10 @@ package com.rabbitminers.extendedbogeys.mixin;
 import com.rabbitminers.extendedbogeys.mixin_interface.BlockStates;
 import com.rabbitminers.extendedbogeys.mixin_interface.ICarriageBogeyStyle;
 import com.simibubi.create.content.logistics.trains.*;
+import com.simibubi.create.content.logistics.trains.entity.Carriage;
 import com.simibubi.create.content.logistics.trains.entity.CarriageBogey;
 import com.simibubi.create.content.logistics.trains.entity.CarriageContraption;
+import com.simibubi.create.content.logistics.trains.entity.Train;
 import com.simibubi.create.content.logistics.trains.management.edgePoint.station.StationTileEntity;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
@@ -14,6 +16,7 @@ import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -23,35 +26,47 @@ import java.util.UUID;
 
 @Mixin(StationTileEntity.class)
 public class MixinStationTileEntity {
+    private CarriageContraption contraption;
 
-    // TODO: Find a way to limit the captured local variables
-    @Inject(
-        method = "assemble",
-        locals = LocalCapture.CAPTURE_FAILHARD,
-        at = @At(
-            value = "INVOKE",
-            target = "Lcom/simibubi/create/content/logistics/trains/entity/Carriage;<init>(Lcom/simibubi/create/content/logistics/trains/entity/CarriageBogey;Lcom/simibubi/create/content/logistics/trains/entity/CarriageBogey;I)V",
-            shift = At.Shift.BEFORE
-        ),
-        remap = false
-    )
-    public void assembleFirstBogey(UUID playerUUID, CallbackInfo ci, BlockPos trackPosition, BlockState trackState, ITrackBlock track, BlockPos bogeyOffset, TrackNodeLocation location, Vec3 centre, Collection ends, Vec3 targetOffset, List pointOffsets, int iPrevious, List points, Vec3 directionVec, TrackGraph graph, TrackNode secondNode, List contraptions, List carriages, List spacing, boolean atLeastOneForwardControls, int bogeyIndex, int pointIndex, CarriageContraption contraption, BlockPos bogeyPosOffset, IBogeyBlock typeOfFirstBogey, CarriageBogey firstBogey, CarriageBogey secondBogey, BlockPos secondBogeyPos, int bogeySpacing) {
+    @ModifyVariable(method = "assemble", at = @At("STORE"), name = "contraption", remap = false)
+    public CarriageContraption captureCarriageContraptionOnInit(CarriageContraption carriageContraption) {
+        this.contraption = carriageContraption;
+        return carriageContraption;
+    }
+
+    @ModifyVariable(method = "assemble", at = @At("STORE"), name = "firstBogey", remap = false)
+    public CarriageBogey onFirstBogeyInit(CarriageBogey firstBogey) {
         Level level = Minecraft.getInstance().level;
-        assert level != null;
+        if (contraption == null || level == null)
+            return firstBogey;
 
         if (firstBogey instanceof ICarriageBogeyStyle styledCustomBogey) {
             BlockPos firstBogeyBlockPos = contraption.anchor;
             BlockState state = level.getBlockState(firstBogeyBlockPos);
             int firstBogeyStyle = state.getValue(BlockStates.STYLE);
             styledCustomBogey.setStyle(firstBogeyStyle);
+            return (CarriageBogey) styledCustomBogey;
         }
 
-        if (secondBogey != null) {
-            BlockState secondBogeyState = level.getBlockState(secondBogeyPos);
-            int secondBogeyStyle = secondBogeyState.getValue(BlockStates.STYLE);
-            if (secondBogey instanceof ICarriageBogeyStyle styledCustomBogey)
-                styledCustomBogey.setStyle(secondBogeyStyle);
-        }
+        return firstBogey;
     }
 
+    @ModifyVariable(method = "assemble", at = @At("STORE"), name = "secondBogey", remap = false)
+    public CarriageBogey onSecondBogeyInit(CarriageBogey secondBogey) {
+        Level level = Minecraft.getInstance().level; // Only needs to read data so client side is safe
+        if (contraption == null || level == null)
+            return secondBogey;
+
+        if (secondBogey != null) {
+            BlockPos secondBogeyPos = contraption.getSecondBogeyPos();
+            BlockState secondBogeyState = level.getBlockState(secondBogeyPos);
+            int secondBogeyStyle = secondBogeyState.getValue(BlockStates.STYLE);
+            if (secondBogey instanceof ICarriageBogeyStyle styledCustomBogey) {
+                styledCustomBogey.setStyle(secondBogeyStyle);
+                return (CarriageBogey) styledCustomBogey;
+            }
+        }
+
+        return secondBogey;
+    }
 }
