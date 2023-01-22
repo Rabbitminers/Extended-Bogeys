a2b4c7a80b91f5fd7f7e553f72042d1615310454
Charlie Wood <rabbitminers2.0@gmail.com>
2023-01-22 19:03:35 +0000

    Added Inworld Rendering For Single Axis Bogey

---
+++ src/main/java/com/rabbitminers/extendedbogeys/bogey/styles/SingleAxisBogey.java
@@ -0,0 +1,46 @@
+package com.rabbitminers.extendedbogeys.bogey.styles;
+
+import com.jozufozu.flywheel.api.MaterialManager;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.rabbitminers.extendedbogeys.index.BogeyPartials;
+import com.simibubi.create.AllBlockPartials;
+import com.simibubi.create.foundation.gui.element.GuiGameElement;
+import com.simibubi.create.foundation.render.CachedBufferer;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.List;
+
+public class SingleAxisBogey implements IBogeyStyle {
+    private static final String STYLE_NAME = "Single Axis";
+    @Override
+    public void renderLargeInWorld(float wheelAngle, PoseStack ms, int light, VertexConsumer vb, BlockState air) {
+        IBogeyStyle.super.renderLargeInWorld(wheelAngle, ms, light, vb, air);
+    }
+    @Override
+    public void renderSmallInWorld(float wheelAngle, PoseStack ms, int light, VertexConsumer vb, BlockState air) {
+        CachedBufferer.partial(BogeyPartials.SINGLE_AXEL_LEADING_TRUCK_FRAME, air)
+                .scale(1 - 1/512f)
+                .light(light)
+                .renderInto(ms, vb);
+
+        CachedBufferer.partial(BogeyPartials.SINGLE_AXEL_LEADING_TRUCK_PIN, air)
+                .scale(1 - 1 / 512f)
+                .light(light)
+                .renderInto(ms, vb);
+
+        ms.pushPose();
+
+        CachedBufferer.partial(AllBlockPartials.SMALL_BOGEY_WHEELS, air)
+                .translate(0, 12 / 16f, -0.25)
+                .rotateX(wheelAngle)
+                .light(light)
+                .renderInto(ms, vb);
+
+        ms.popPose();
+    }
+    @Override
+    public String getStyleName() {
+        return STYLE_NAME;
+    }
+}
