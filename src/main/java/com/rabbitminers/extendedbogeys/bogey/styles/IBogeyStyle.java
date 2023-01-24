0491878f0e35330c055d9fc51c1140652ef24839
Charlie Wood <rabbitminers2.0@gmail.com>
2023-01-24 07:03:19 +0000

    Refactored Method Names To More Closely Match Their Functionality

--- src/main/java/com/rabbitminers/extendedbogeys/bogey/styles/IBogeyStyle.java
+++ src/main/java/com/rabbitminers/extendedbogeys/bogey/styles/IBogeyStyle.java
@@ -1,18 +1,13 @@
 package com.rabbitminers.extendedbogeys.bogey.styles;
 
-import com.jozufozu.flywheel.api.InstanceData;
 import com.jozufozu.flywheel.api.MaterialManager;
-import com.jozufozu.flywheel.core.Materials;
-import com.jozufozu.flywheel.core.PartialModel;
 import com.jozufozu.flywheel.core.materials.model.ModelData;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.simibubi.create.AllBlockPartials;
+import com.simibubi.create.foundation.config.AllConfigs;
 import com.simibubi.create.foundation.gui.element.GuiGameElement;
 import net.minecraft.world.level.block.state.BlockState;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import javax.annotation.meta.When;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,13 +22,13 @@ public interface IBogeyStyle {
         if (isLarge) renderLargeInWorld(wheelAngle, ms, light, vb, air); else renderSmallInWorld(wheelAngle, ms, light, vb, air);
     }
     public default void renderInContraption(boolean isLarge, MaterialManager materialManager) {
-        if (isLarge) renderLargeInContraption(materialManager); else renderSmallInContraption(materialManager);
+        if (isLarge) registerLargeBogeyModelData(materialManager); else registerSmallBogeyModelData(materialManager);
     }
     public default List<GuiGameElement.GuiRenderBuilder> renderInGuiOverlay(boolean isLarge) {
         if (isLarge) return renderLargeInGuiOverlay(); else return renderSmallInGuiOverlay();
     }
     public default void beginFrame(boolean isLarge, float wheelAngle, PoseStack ms) {
-        if (isLarge) beginFrameLarge(wheelAngle, ms); else beginFrameSmall(wheelAngle, ms);
+        if (isLarge) animateLargeBogeyInContraption(wheelAngle, ms); else animateSmallBogeyInContraption(wheelAngle, ms);
     }
     default void setEmptyTransforms() {
         for (ModelData customModelComponent : getAllCustomModelComponents())
@@ -56,15 +51,18 @@ public interface IBogeyStyle {
     default float getMinimumTurnRadius() {
         return 0.0f;
     }
+    default float getMaximumSpeed() {
+        return AllConfigs.SERVER.trains.trainTopSpeed.getF();
+    }
     default List<ModelData> getAllCustomModelComponents() {
         return new ArrayList<>();
     }
     default List<GuiGameElement.GuiRenderBuilder> renderLargeInGuiOverlay() {return new ArrayList<>();}
     default List<GuiGameElement.GuiRenderBuilder> renderSmallInGuiOverlay() {return new ArrayList<>();}
-    default void beginFrameLarge(float wheelAngle, PoseStack ms) {}
-    default void beginFrameSmall(float wheelAngle, PoseStack ms) {}
+    default void animateLargeBogeyInContraption(float wheelAngle, PoseStack ms) {}
+    default void animateSmallBogeyInContraption(float wheelAngle, PoseStack ms) {}
     default void renderLargeInWorld(float wheelAngle, PoseStack ms, int light, VertexConsumer vb, BlockState air) {}
     default void renderSmallInWorld(float wheelAngle, PoseStack ms, int light,  VertexConsumer vb, BlockState air) {}
-    default void renderLargeInContraption(MaterialManager materialManager) {}
-    default void renderSmallInContraption(MaterialManager materialManager) {}
+    default void registerLargeBogeyModelData(MaterialManager materialManager) {}
+    default void registerSmallBogeyModelData(MaterialManager materialManager) {}
 }
