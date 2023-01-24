3f5d0fbffa713bc9265e4e3645a91a244488efc8
Charlie Wood <rabbitminers2.0@gmail.com>
2023-01-24 17:12:21 +0000

    Implemented Direction to ICarriageBogeyStyle

--- src/main/java/com/rabbitminers/extendedbogeys/mixin_interface/ICarriageBogeyStyle.java
+++ src/main/java/com/rabbitminers/extendedbogeys/mixin_interface/ICarriageBogeyStyle.java
@@ -2,5 +2,11 @@ package com.rabbitminers.extendedbogeys.mixin_interface;
 
 public interface ICarriageBogeyStyle {
     default int getStyle() { return 0; }
+
     default void setStyle(int style) {}
+
+    default boolean isFacingForward() { return true; }
+
+    default void setFacingForward(boolean isFacingForward) {}
+
 }
