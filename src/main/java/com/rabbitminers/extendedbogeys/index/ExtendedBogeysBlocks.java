1b4260f86112ea1ebeabbd4c5c18d5da1c78126a
Charlie Wood <rabbitminers2.0@gmail.com>
2023-02-09 16:57:21 +0000

    Registered Custom Bogey Sizes

--- src/main/java/com/rabbitminers/extendedbogeys/index/ExtendedBogeysBlocks.java
+++ src/main/java/com/rabbitminers/extendedbogeys/index/ExtendedBogeysBlocks.java
@@ -1,10 +1,13 @@
 package com.rabbitminers.extendedbogeys.index;
 
 import com.rabbitminers.extendedbogeys.ExtendedBogeys;
+import com.rabbitminers.extendedbogeys.bogey.sizes.CustomSizeBogeyBlock;
 import com.rabbitminers.extendedbogeys.bogey.unlinked.UnlinkedBogeyCarriageMovementBehaviour;
 import com.rabbitminers.extendedbogeys.bogey.unlinked.UnlinkedStandardBogeyBlock;
+import com.rabbitminers.extendedbogeys.bogey.util.BogeyBlockList;
 import com.simibubi.create.AllBlocks;
 import com.simibubi.create.AllTags;
+import com.simibubi.create.content.logistics.trains.track.StandardBogeyBlock;
 import com.simibubi.create.foundation.data.BlockStateGen;
 import com.simibubi.create.foundation.data.BuilderTransformers;
 import com.simibubi.create.foundation.data.CreateRegistrate;
@@ -12,34 +15,36 @@ import com.tterrag.registrate.util.entry.BlockEntry;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.material.MaterialColor;
 
+import java.util.List;
+
 import static com.simibubi.create.AllMovementBehaviours.movementBehaviour;
+import static com.simibubi.create.Create.REGISTRATE;
 
 public class ExtendedBogeysBlocks {
     private static final CreateRegistrate REGISTRATE = ExtendedBogeys.registrate();
 
-    public static final BlockEntry<UnlinkedStandardBogeyBlock> SMALL_UNLINKED_BOGEY =
-            REGISTRATE.block("small_unlinked_bogey", p -> new UnlinkedStandardBogeyBlock(p, false))
-                    .properties(p -> p.color(MaterialColor.PODZOL))
-                    .properties(p -> p.sound(SoundType.NETHERITE_BLOCK))
-                    .properties(p -> p.noOcclusion())
-                    .transform(AllTags.pickaxeOnly())
-                    .blockstate((c, p) -> BlockStateGen.horizontalAxisBlock(c, p, s -> p.models()
-                            .getExistingFile(p.modLoc("block/track/bogey/unlinked_top"))))
-                    .loot((p, l) -> p.dropOther(l, AllBlocks.RAILWAY_CASING.get()))
-                    .onRegister(movementBehaviour(new UnlinkedBogeyCarriageMovementBehaviour()))
-                    .register();
+    // Extra Bogeys
 
-    public static final BlockEntry<UnlinkedStandardBogeyBlock> LARGE_UNLINKED_BOGEY =
-            REGISTRATE.block("large_unlinked_bogey", p -> new UnlinkedStandardBogeyBlock(p, true))
-                    .properties(p -> p.color(MaterialColor.PODZOL))
-                    .properties(p -> p.sound(SoundType.NETHERITE_BLOCK))
-                    .properties(p -> p.noOcclusion())
-                    .transform(AllTags.pickaxeOnly())
-                    .blockstate((c, p) -> BlockStateGen.horizontalAxisBlock(c, p, s -> p.models()
-                            .getExistingFile(p.modLoc("block/track/bogey/unlinked_top"))))
-                    .loot((p, l) -> p.dropOther(l, AllBlocks.RAILWAY_CASING.get()))
-                    .onRegister(movementBehaviour(new UnlinkedBogeyCarriageMovementBehaviour()))
-                    .register();
+    public static final BogeyBlockList<? extends StandardBogeyBlock> CUSTOM_BOGEYS = new BogeyBlockList<>(size -> {
+       if (!size.isDefault()) { // Don't re-register existing bogey blocks
+           return REGISTRATE.block(size.getName() + "_bogey", p -> new CustomSizeBogeyBlock(p, size))
+                   .properties(p -> p.color(MaterialColor.PODZOL))
+                   .transform(BuilderTransformers.bogey())
+                   .register();
+       }
+       return null;
+    });
+
+    public static final BogeyBlockList<? extends UnlinkedStandardBogeyBlock> UNLINKED_BOGEYS = new BogeyBlockList<>(size -> REGISTRATE.block(size.getName() + "_unlinked_bogey", p -> new UnlinkedStandardBogeyBlock(p, false))
+            .properties(p -> p.color(MaterialColor.PODZOL))
+            .properties(p -> p.sound(SoundType.NETHERITE_BLOCK))
+            .properties(p -> p.noOcclusion())
+            .transform(AllTags.pickaxeOnly())
+            .blockstate((c, p) -> BlockStateGen.horizontalAxisBlock(c, p, s -> p.models()
+                    .getExistingFile(p.modLoc("block/track/bogey/unlinked_top"))))
+            .loot((p, l) -> p.dropOther(l, AllBlocks.RAILWAY_CASING.get()))
+            .onRegister(movementBehaviour(new UnlinkedBogeyCarriageMovementBehaviour()))
+            .register());
 
     public static void register() {}
 }
