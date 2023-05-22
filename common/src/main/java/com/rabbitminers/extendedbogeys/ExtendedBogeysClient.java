c96dd3e36621ad5655e2c79cc8c25c912a7aebf8
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-05-22 20:54:59 +0100

    Added client init

---
+++ common/src/main/java/com/rabbitminers/extendedbogeys/ExtendedBogeysClient.java
@@ -0,0 +1,9 @@
+package com.rabbitminers.extendedbogeys;
+
+import com.rabbitminers.extendedbogeys.registry.ExtendedBogeysPartials;
+
+public class ExtendedBogeysClient {
+    public static void init() {
+        ExtendedBogeysPartials.init();
+    }
+}
