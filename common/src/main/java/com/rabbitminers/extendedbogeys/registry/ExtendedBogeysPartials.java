94caf4b305353be2ef9b97aa5b45baa684cf9f54
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-01 00:41:09 +0100

    Registered new painted partials

--- common/src/main/java/com/rabbitminers/extendedbogeys/registry/ExtendedBogeysPartials.java
+++ common/src/main/java/com/rabbitminers/extendedbogeys/registry/ExtendedBogeysPartials.java
@@ -2,50 +2,93 @@ package com.rabbitminers.extendedbogeys.registry;
 
 import com.jozufozu.flywheel.core.PartialModel;
 import com.rabbitminers.extendedbogeys.ExtendedBogeys;
+import com.rabbitminers.extendedbogeys.data.BogeyPaintColour;
+import com.simibubi.create.AllPartialModels;
+
+import java.util.EnumMap;
+import java.util.Map;
 
 public class ExtendedBogeysPartials {
     public static final PartialModel
 
-            // Single Axle
+    // Single Axle
 
-            // Small (2-0-0 / 0-0-2)
-            SMALL_SINGLE_AXLE_FRAME = block("singleaxle/small/single_axel_leading_truck_frame"),
+    // Small (2-0-0 / 0-0-2)
 
-            SMALL_SINGLE_AXLE_PIN = block("singleaxle/small/single_axel_leading_truck_pin"),
+    SMALL_SINGLE_AXLE_FRAME = block("singleaxle/small/single_axel_leading_truck_frame"),
 
-            // Double Axle
+    SMALL_SINGLE_AXLE_PIN = block("singleaxle/small/single_axel_leading_truck_pin"),
 
-            // Small (4-0-0 / 0-0-4)
-            SMALL_DOUBLE_AXLE_FRAME = block("doubleaxle/small/frame"),
-            SMALL_DOUBLE_AXLE_PIN = block("doubleaxle/small/pin"),
+    // Double Axle
 
-            // Large (0-4-0)
-            LARGE_DOUBLE_AXLE_DRIVER_ROD = block("doubleaxle/large/driverod"),
+    // Small (2-0-0 / 0-0-2)
+    SMALL_DOUBLE_AXLE_FRAME = block("doubleaxle/small/frame"),
+    SMALL_DOUBLE_AXLE_PIN = block("doubleaxle/small/pin"),
+
+    // Large (0-4-0)
+    LARGE_DOUBLE_AXLE_DRIVER_ROD = block("doubleaxle/large/driverod"),
             LARGE_DOUBLE_AXLE_DRIVER_PIN = block("doubleaxle/large/drivepin"),
             LARGE_DOUBLE_AXLE_DRIVER_CONNECTING_ROD = block("doubleaxle/large/connectingrod"),
-            LARGE_DOUBLE_AXLE_DRIVER_FRAME = block("doubleaxle/large/frame"),
 
-            // Triple Axle
+    // Triple Axle
 
-            // Large (0-6-0)
-            LARGE_TRIPLE_AXLE_DRIVER_FRAME = block("tripleaxle/large/frame"),
-            LARGE_TRIPLE_AXLE_DRIVER_CONNECTING_ROD = block("tripleaxle/large/connectingrod"),
+    // Large (0-6-0)
+    LARGE_TRIPLE_AXLE_DRIVER_CONNECTING_ROD = block("tripleaxle/large/connectingrod"),
             LARGE_TRIPLE_AXLE_DRIVER_PIN = block("tripleaxle/large/drivepin"),
             LARGE_TRIPLE_AXLE_DRIVER_ROD = block("tripleaxle/large/driverod"),
             LARGE_TRIPLE_AXLE_DRIVER_ECCENTRIC = block("tripleaxle/large/eccentric"),
             LARGE_TRIPLE_AXLE_DRIVER_ECCENTRIC_ROD = block("tripleaxle/large/eccentricrod"),
             LARGE_TRIPLE_AXLE_DRIVER_RADIUSROD = block("tripleaxle/large/radiusrod")
+    ;
 
-            ;
+    public static final Map<BogeyPaintColour, PartialModel>
+            // Overwrites of existing models
 
+            SMALL_WHEELS = new EnumMap<>(BogeyPaintColour.class),
+            LARGE_WHEELS = new EnumMap<>(BogeyPaintColour.class),
+
+            // Large (0-4-0)
+
+            LARGE_DOUBLE_AXLE_DRIVER_FRAMES = dyed("doubleaxle/large/frame"),
+
+            // Triple Axle
+
+            // Large (0-6-0)
+            LARGE_TRIPLE_AXLE_DRIVER_FRAMES = dyed("tripleaxle/large/frame")
+    ;
+
+    static {
+        for (BogeyPaintColour colour : BogeyPaintColour.values()) {
+            if (colour == BogeyPaintColour.UNPAINTED) {
+                SMALL_WHEELS.put(BogeyPaintColour.UNPAINTED, AllPartialModels.SMALL_BOGEY_WHEELS);
+                LARGE_WHEELS.put(BogeyPaintColour.UNPAINTED, AllPartialModels.LARGE_BOGEY_WHEELS);
+            } else {
+                SMALL_WHEELS.put(colour, dyed(colour, "/bogey/wheel"));
+                LARGE_WHEELS.put(colour, dyed(colour,"/bogey/drive_wheel"));
+            }
+        }
+    }
+
+    private static Map<BogeyPaintColour, PartialModel> dyed(String path) {
+        Map<BogeyPaintColour, PartialModel> MAP = new EnumMap<>(BogeyPaintColour.class);
+        for(BogeyPaintColour colour : BogeyPaintColour.values())
+            MAP.put(colour, dyed(colour, path));
+        return MAP;
+    }
+
+    private static PartialModel dyed(BogeyPaintColour colour, String path) {
+        return new PartialModel(ExtendedBogeys.asResource(colour.name + "/block/" + path));
+    }
 
     private static PartialModel block(String path) {
-        return new PartialModel(ExtendedBogeys.asResource("block/" + path));
+        return new PartialModel(ExtendedBogeys.asResource("unpainted/block/" + path));
     }
 
     private static PartialModel entity(String path) {
         return new PartialModel(ExtendedBogeys.asResource("entity/" + path));
     }
+
+
 
     public static void init() {
 
