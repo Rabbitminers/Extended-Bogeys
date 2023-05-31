6601401831532d1be5b766d59a7fe90a36808fb7
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-01 00:42:03 +0100

    Added Constants

---
+++ common/src/main/java/com/rabbitminers/extendedbogeys/base/Constants.java
@@ -0,0 +1,7 @@
+package com.rabbitminers.extendedbogeys.base;
+
+public final class Constants {
+    public static final String BOGEY_DIRECTION_KEY = "IsForwards";
+    public static final String BOGEY_PAINT_KEY = "Paint";
+    public static final String BOGEY_ASSEMBLY_DIRECTION_KEY = "AssemblyDirection";
+}
