b47c8904f56e1d19fdfa2f2a1909fa64cfe9c48c
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-02 00:34:08 +0100

    Implemented Carriage Contraption Helpers

---
+++ common/src/main/java/com/rabbitminers/extendedbogeys/multiloader/CarriageContraptionHelper.java
@@ -0,0 +1,11 @@
+package com.rabbitminers.extendedbogeys.multiloader;
+
+import com.simibubi.create.content.trains.entity.CarriageContraptionEntity;
+import dev.architectury.injectables.annotations.ExpectPlatform;
+
+public class CarriageContraptionHelper {
+    @ExpectPlatform
+    public static double getDistanceTo(CarriageContraptionEntity cce) {
+        throw new AssertionError();
+    }
+}
