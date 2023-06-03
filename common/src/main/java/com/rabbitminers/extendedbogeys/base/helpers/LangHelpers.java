aa435d772a999e89f6b6e80259188914b59b6f1a
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-03 10:51:41 +0100

    Added Language Helper Methods

---
+++ common/src/main/java/com/rabbitminers/extendedbogeys/base/helpers/LangHelpers.java
@@ -0,0 +1,13 @@
+package com.rabbitminers.extendedbogeys.base.helpers;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class LangHelpers {
+    public static String capitalize(String line) {
+        return Arrays.stream(line.split("_"))
+                .filter(word -> !word.isEmpty())
+                .map(word -> word.substring(0, 1).toUpperCase() + word.substring(1))
+                .collect(Collectors.joining(" "));
+    }
+}
