9a95d793b9b2f029c332e5df1a2a33af91470806
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-06-19 23:05:51 +0100

    Added Multiloader Handlers to Fabric

---
+++ fabric/src/main/java/com/rabbitminers/extendedbogeys/multiloader/fabric/CarriageContraptionHelperImpl.java
@@ -0,0 +1,13 @@
+package com.rabbitminers.extendedbogeys.multiloader.fabric;
+
+import com.rabbitminers.extendedbogeys.mixin_interface.ICarriageContraptionEntity;
+import com.simibubi.create.content.trains.entity.CarriageContraptionEntity;
+
+public class CarriageContraptionHelperImpl {
+    // Why do we need this? I have no clue, for some reason doing this in common causes forge specific imports to be loaded?
+    public static double getDistanceTo(CarriageContraptionEntity cce) {
+        if (cce instanceof ICarriageContraptionEntity carriageContraptionEntity)
+            return carriageContraptionEntity.getDistanceTo();
+        return 0;
+    }
+}
